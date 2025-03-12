9d5ec02c6beb666a540de678b159b1270005dfc1
denismatvieiev <matvieiev.denys@student.uzhnu.edu.ua>
2025-03-12 14:20:37 +0200

    feat(UserProfil): add mode for various components

--- src/pages/UserProfil/userProfil.tsx
+++ src/pages/UserProfil/userProfil.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './userProfil.module.css'
 import { Settings, Keyboard, LogOut, LayoutDashboard, Flame, PanelsRightBottom, Zap } from 'lucide-react'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 interface Props {
   className?: string
 }
 
 export const UserProfil: React.FC<Props> = ({ className }) => {
+  const [searchParams] = useSearchParams()
+  const mode = searchParams.get('mode')
+  console.log('mode:', mode)
+
   return (
     <div className={styles.container}>
       <div className={styles.infospace}>
@@ -18,31 +22,37 @@ export const UserProfil: React.FC<Props> = ({ className }) => {
           </div>
           <img src='/default_user.png' className={styles.defaultImage} alt='' />
           <div className={styles.navigate}>
-            <Link to='' className={styles.text_flex}>
+            <Link to='?mode=home' className={styles.text_flex}>
               <LayoutDashboard className={styles.dashbord} />
               <p> Дім</p>
             </Link>
           </div>
           <div className={styles.navigate}>
-            <Link to='' className={styles.text_flex}>
+            <Link to='?mode=intensity' className={styles.text_flex}>
               <Flame className={styles.flame} />
               <p>Інтенсивність</p>
             </Link>
           </div>
           <div className={styles.navigate}>
-            <Link to='' className={styles.text_flex}>
+            <Link to='?mode=projects' className={styles.text_flex}>
               <PanelsRightBottom className={styles.rightBottom} />
-              <p> Проєкти</p>
+              <p>Проєкти</p>
             </Link>
           </div>
           <div className={styles.navigate}>
-            <Link to='' className={styles.text_flex}>
+            <Link to='?mode=courses' className={styles.text_flex}>
               <Zap className={styles.zap} />
-              <p> Мастер класи</p>
+              <p>Мастер класи</p>
             </Link>
           </div>
         </div>
-        <div className={styles.rightSideInfo}></div>
+
+        <div className={styles.rightSideInfo}>
+          {mode === 'home' && <p>Home</p>}
+          {mode === 'intensity' && <p>intensity</p>}
+          {mode === 'projects' && <p>projects</p>}
+          {mode === 'courses' && <p>courses</p>}
+        </div>
       </div>
     </div>
   )
